82db25f1c500919b2e47852f5a93c8fac5771dae
waqar903 <37924887+waqar903@users.noreply.github.com>
2019-09-19 18:04:57 +0500

    Initialize the project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'first-with-tabs', loadChildren: './pages/first-with-tabs/first-with-tabs.module#FirstWithTabsPageModule' },
+  { path: 'second', loadChildren: './pages/second/second.module#SecondPageModule' },
+  { path: 'tab1', loadChildren: './pages/tab1/tab1.module#Tab1PageModule' },
+  { path: 'tab2', loadChildren: './pages/tab2/tab2.module#Tab2PageModule' },
+  { path: 'details', loadChildren: './pages/details/details.module#DetailsPageModule' },
+  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
